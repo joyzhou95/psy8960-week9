f7e1f89202e9eb64c3803b83d5086f56d2be799f
Joy Zhou <zhou1559@umn.edu>
2023-03-23 17:20:37 -0500

    Select the data sublist

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,12 +1,19 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(jsonlite)
-library(rvest)
+library(rlist)
+library(tidyverse)
 
 # Data Import and Cleaning
 
 ## Used fromJSON to read the json file into a list that can be manipulated in R
 rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json")
+
+##Used list.select to select only the data list under children
+##Needed to wrap rstats_list with list() to create a list environment for list.select to work
+rstats_original_tbl <- list.select(list(rstats_list), data$children$data)
+
+
 
 
 
