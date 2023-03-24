085b8d4220be7b09b71195655a6056f52bffa958
Joy Zhou <zhou1559@umn.edu>
2023-03-24 15:10:17 -0500

    Scraped upvotes

--- R/reddit_scraped.R.R
+++ R/reddit_scraped.R.R
@@ -11,3 +11,8 @@ post_title <- rstats_html %>%
   html_elements(xpath = 'p[@class = "title"]/a') %>%
   html_text()
 
+rstats_html %>%
+  html_elements(xpath = '//div[p[text() = "submitted "]]') %>%
+  html_elements(xpath = '..//preceding-sibling::div[@class = "midcol unvoted"]') %>%
+  html_elements(xpath = 'div[@class = "score unvoted"]') %>%
+  html_text()
