040e05297da32ece8449d66deab799428265e97c
Joy Zhou <zhou1559@umn.edu>
2023-03-24 10:31:20 -0500

    Created a variable contains only post titles

--- R/reddit_scraped.R.R
+++ R/reddit_scraped.R.R
@@ -1,10 +1,13 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
 library(rvest)
 
-
 # Data Import and Cleaning
+rstats_html <-read_html("https://old.reddit.com/r/rstats/")
 
-rstats_html <- read_html("https://old.reddit.com/r/rstats/")
-
+post_title <- rstats_html %>%
+  html_elements(xpath = '//div[p[text() = "submitted "]]') %>%
+  html_elements(xpath = 'p[@class = "title"]/a') %>%
+  html_text()
 
